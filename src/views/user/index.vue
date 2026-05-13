d772c396d32bad556c5bcb91d1c5818954e375d1
xiaohuanemiya <emiya172625@gmail.com>
2026-05-13 16:57:53 +0800

    fix: 更新用户保存接口，确保邮箱字段为必填项并添加验证规则

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -174,6 +174,7 @@ const rules: FormRules = {
     { min: 6, max: 50, message: '密码长度为6-50个字符', trigger: 'blur' }
   ],
   email: [
+    { required: true, message: '请输入邮箱', trigger: 'blur' },
     { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
   ]
 }
